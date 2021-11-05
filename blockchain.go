8e63bb620025e7df1445fdee6430581de75c21da
Alex Badia <badia.alex.h@gmail.com>
2021-11-05 19:44:18 -0400

    Added create end point

--- blockchain.go
+++ blockchain.go
@@ -1,1 +1,32 @@
 package main
+
+//================================================================================
+// Certificate Type Enums
+//================================================================================
+
+const (
+	education          string = "Education"
+	employment         string = "Employement"
+	tradeCertification string = "Trade Certification"
+	other              string = "Other"
+)
+
+//================================================================================
+// Blockchain Structs
+//================================================================================
+
+type Block struct {
+	UserId              string `json:"userId"`
+	CertificateToken    string `json:"certificate_token"`
+	CertificateUrl      string `json:"certificate_url"`
+	CertificateCatgeory string `json:"certificate_category"`
+	InstitionName       string `json:"institution_name"`
+	DegreeName          string `json:"degree_name"`
+	DateRange           string `json:"date_range"`
+	Description         string `json:"description"`
+} // Block
+
+type Blockchain struct {
+	userId int
+	chain  []Block
+} // Blockchain
--- main.go
+++ main.go
@@ -1,21 +1,91 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 )
 
+// Map to store all users blockchains
+var UserChains = make(map[string]Blockchain)
+
+// Returns the current status of the server
 func status(w http.ResponseWriter, r *http.Request) {
 	log.Println("\"alive\":true")
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"alive": true}`)
 } // status
 
+// Create endpoint
+func create(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/create" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	} // if
+
+	switch r.Method {
+	// Serve create.html
+	case "GET":
+		http.ServeFile(w, r, "templates/create.html")
+	// Parse the form
+	case "POST":
+		// Parse JSON to Block Struct
+		var bloc Block
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		} // if
+		if err := json.Unmarshal(body, &bloc); err != nil {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		} // if
+
+		// Check for user's chain
+		userChain, exist := UserChains[bloc.UserId]
+		if !exist {
+			// Add new User Blockchain
+			var newBlockchain *Blockchain = new(Blockchain)
+			UserChains[bloc.UserId] = *newBlockchain
+		} // if
+
+		// Add to chain
+		userChain.chain = append(userChain.chain, bloc)
+
+		fmt.Printf("%v\n", UserChains)
+		// // Parse Form
+		//
+		// if err := r.ParseForm(); err != nil {
+		// 	fmt.Fprintf(w, "ParseForm() err: %v", err)
+		// 	return
+		// } // if
+		//
+		// block := Block{
+		// 	InstitionName: r.FormValue("institution_name"),
+		// 	DegreeName:    r.FormValue("degree_name"),
+		// 	DateRange:     r.FormValue("dates"),
+		// 	Description:   r.FormValue("description"),
+		// } // Block
+		//
+		// if blockJson, err := json.Marshal(&block); err == nil {
+		// 	w.Write(blockJson)
+		// } // if
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+	} // switch
+} // newBlock
+
 func main() {
-	port := os.Getenv("PORT")
+	// TODO: Run locally
+	port := "8000"
+
+	// TODO: For production
+	// port := os.Getenv("PORT")
 	http.HandleFunc("/", status)
+	http.HandleFunc("/create", create)
 	log.Print("Listening on port: " + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 } // main
