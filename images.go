2a5e53929f7979c76d0520089d3e6849d4647f01
Alex Badia <badia.alex.h@gmail.com>
2021-11-09 20:31:46 -0500

    Added certified image

--- images.go
+++ images.go
@@ -9,6 +9,8 @@ func getImage(w http.ResponseWriter, r *http.Request) {
 
 	if name := r.URL.Query().Get("name"); name == "bobby" {
 		http.ServeFile(w, r, "images/bobby.png")
+	} else if name := r.URL.Query().Get("name"); name == "certified" {
+		http.ServeFile(w, r, "images/certified.png")
 	} else {
 		http.Error(w, "Image doesn't exist of exists!", http.StatusNoContent)
 	} // else
