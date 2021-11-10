fe36db151148813ad902c04789ad16ef82d4e81c
Alex Badia <badia.alex.h@gmail.com>
2021-11-09 19:17:11 -0500

    forgot to add content-type to http header

--- images.go
+++ images.go
@@ -6,16 +6,11 @@ import "net/http"
 func getImage(w http.ResponseWriter, r *http.Request) {
 	// This is bad, I know, it's for demo purposes only...
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	// Make sure this endpoint is only accessible at "/image"
-	// if r.URL.Path != "/image" {
-	// 	http.Error(w, "Bad request", http.StatusBadRequest)
-	// 	return
-	// } // if
+	w.Header().Set("Content-Type", "image/png")
 
 	if name := r.URL.Query().Get("name"); name == "bobby" {
 		http.ServeFile(w, r, "images/bobby-headshot.png")
 	} else {
-		http.Error(w, "No Image of "+name+" exists!", http.StatusNoContent)
+		http.Error(w, "Image doesn't exist of exists!", http.StatusNoContent)
 	} // else
 } // getImage
